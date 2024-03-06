fb912466ce1c86fe59ff3786b0713cc2eb38306a
Iri O <oparina.iri@gmail.com>
2024-03-06 20:53:19 +0600

    Changed function main, created function print_links

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
-from service import get_links_from_url
+from service import get_links_from_url, get_domain_name
 
 
-def main():
-    links = get_links_from_url('https://en.wikipedia.org/wiki/Main_Page')
-    print(links)
+def print_links(links: set, indent: int):
+    whitespace = ' '*indent
+    for link in links:
+        print(f'{whitespace}{link}')
+
+
+# def main(url: str, depth: int = 3, indent: int = 0):
+#     while depth > 1:
+#         links = get_links_from_url(url=url)
+#         whitespace = ' ' * indent
+#         print(f'{whitespace}{url}: ')
+#
+#         depth -= 1
+#         indent += 5
+#
+#         if not links or depth == 1:
+#             print_links(links=links, indent=indent)
+#             break
+#
+#         for link in links:
+#             main(url=link, depth=depth, indent=indent)
+#         depth = 1
+#
+
+def main(url: str, depth: int = 3, indent: int = 0):
+    stack = [(url, depth, indent)]
+
+    while stack:
+        url, depth, indent = stack.pop()
+        links = get_links_from_url(url=url)
+        whitespace = ' ' * indent
+        print(f'{whitespace}{url}: ')
+        depth -= 1
+        indent += 5
+
+        if not links or depth == 1:
+            print_links(links=links, indent=indent)
+            continue
+
+        for link in links:
+            stack.append((link, depth, indent))
 
 
 if __name__ == '__main__':
-    main()
+    main(url='https://transfonter.org/')
+    main('https://avocode.com/')
 
