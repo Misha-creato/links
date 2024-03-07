609341b5a2c16f7e7892029dfd41d640ef0dd133
Iri O <oparina.iri@gmail.com>
2024-03-07 20:01:01 +0600

    Changed check funcs

--- check.py
+++ check.py
@@ -7,14 +7,16 @@ PATTERN_STR = r'^https?:\/\/(.*\.)*(.*\.\w+)'
 def check_url(url: str):
     if re.match(pattern=PATTERN_STR, string=url):
         return True
+    return False
 
 
-def is_link_internal(link: str, domain_name: str, root_domain_name: str):
-    if link and (link.startswith('/') or root_domain_name == domain_name):
+def is_link_internal(link: str, root_domain_name: str):
+    if root_domain_name in link:
         return True
+    return False
 
 
 def is_link_equal_root_url(link: str, root_url: str):
     if link == root_url:
         return True
-
+    return False
