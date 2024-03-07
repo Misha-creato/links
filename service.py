33bf593b02ed69b11fbc7dc348236f4f6c03239d
Iri O <oparina.iri@gmail.com>
2024-03-07 20:02:35 +0600

    Changed link return func

--- service.py
+++ service.py
@@ -13,17 +13,22 @@ def get_links_from_url(url: str):
     all_link_tags = response_html.find_all('a')
     for link_tag in all_link_tags:
         link = link_tag.get('href')
-        full_link = check_and_return_link(link=link, root_domain_name=root_domain_name, root_url=url)
+
+        full_link = check_and_return_link(
+            link=link,
+            root_domain_name=root_domain_name,
+            root_url=url
+        )
+
         if full_link:
             internal_links.add(full_link)
     return internal_links
 
 
 def check_and_return_link(link: str, root_domain_name: str, root_url: str):
-    domain_name = get_domain_name(url=link)
-    if check.is_link_internal(link=link, domain_name=domain_name, root_domain_name=root_domain_name):
-        if link.startswith('/'):
-            link = make_link_from_path(path=link, root_url=root_url)
+    if link and link.startswith('/'):
+        link = make_link_from_path(path=link, root_url=root_url)
+    if check.is_link_internal(link=link, root_domain_name=root_domain_name):
         if check.is_link_equal_root_url(link=link, root_url=root_url):
             return None
         return link
@@ -36,7 +41,8 @@ def get_domain_name(url: str):
 
 
 def make_link_from_path(path: str, root_url: str):
-    result = re.match(pattern=r"({})".format(check.PATTERN_STR), string=root_url)
+    pattern = f'({check.PATTERN_STR})'
+    result = re.match(pattern=pattern, string=root_url)
     link = result.group(1) + path
     return link
 
